3c672cdda253bd4c9d17f37eb587e3eecc322604
Tim Rijckaert <tim.rijckaert@vrt.be>
2017-08-13 21:18:18 +0200

    Added a little logging library. We will see how useful this turns out to be

--- src/DI/store.js
+++ src/DI/store.js
@@ -1,11 +1,13 @@
-import {createStore, applyMiddleware, combineReducers, compose} from 'redux'
+import { createStore, applyMiddleware, compose } from 'redux'
 import thunk from 'redux-thunk'
 import reducer from '../redux/reducer'
+import { loggerMiddleware } from './utils'
 
 function configureStore(initialState) {
     const enhancer = compose(
         applyMiddleware(
-            thunk
+            thunk,
+            loggerMiddleware
         ),
     );
     return createStore(reducer, initialState, enhancer)
--- src/DI/utils.js
+++ src/DI/utils.js
@@ -1,8 +1,11 @@
-import {DateHelper} from '../util/DateHelper';
-import {lazy} from '../util/lazy';
+import { DateHelper } from '../util/DateHelper';
+import { lazy } from '../util/lazy';
+import { createLogger } from "redux-logger";
 
 const dateHelper = lazy(() => new DateHelper());
+const loggerMiddleware = createLogger({ predicate: (getState, action) => __DEV__ });
 
 export {
-    dateHelper
+    dateHelper,
+    loggerMiddleware
 };
